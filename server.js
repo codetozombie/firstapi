bf0f76667947f3304abac90e6d6733fe7c062784
Jim Isaac Awuah <ijawuah@st.ug.edu.gh>
2023-12-17 13:37:55 +0000

    bd is not working

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const app =  express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
 // routes
+app.get('/', (req, res) => {
+  res.send('Hello, my name is Albert');
+});
 
-app.get('/',(req, res)=>{
-  res.send('Helo Node API')
+app.listen(3000, () => {
+  console.log('Node API app is running on port 3000');
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://orgin11phile:1806@InTheElevator@cluster0.chw2ieu.mongodb.net/?retryWrites=true&w=majority',
   
-})
-
-app.listen(3000,()=>{
-  console.log(`Node APi app is running on port 3000`)
-})
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
